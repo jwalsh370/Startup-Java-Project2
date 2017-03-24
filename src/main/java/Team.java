92c6c730f7f436d1f8a57478a34c646b3568ae13
jahan <jwalsh370@gmail.com>
2017-03-24 09:45:29 -0700

    test TeamName and ID getters, all test continue to pass.

--- src/main/java/Team.java
+++ src/main/java/Team.java
@@ -15,5 +15,32 @@ public class Team {
     mMember = new ArrayList<Member>();
   }
 
+  public String getTeamName() {
+    return mTeamName;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Team find(int id) {
+    return instances.get(id - 1);
+  }
+
+  public static List<Member> getMember() {
+   return mMember;
+ }
+
+ public static void addMember(Member member) {
+   mMember.add(member);
+ }
+
+ public static List<Team> getAllTeams() {
+   return instances;
+ }
 
 }
--- src/test/java/TeamTest.java
+++ src/test/java/TeamTest.java
@@ -3,10 +3,21 @@ import static org.junit.Assert.*;
 
 public class TeamTest {
 
+  @After
+ public void tearDown() {
+   Team.clear();
+ }
+
   @Test
   public void TeamClass_InstantiatesTrue() {
     Team myTeam = new Team("Falcon");
     assertEquals(true, myTeam instanceof Team);
   }
 
+  @Test
+  public void test_TeamGetters() {
+    Team myTeam = new Team("Falcon");
+    assertEquals("Falcon", myTeam.getTeamName());
+    assertEquals(1, myTeam.getId());
+  }
 }
