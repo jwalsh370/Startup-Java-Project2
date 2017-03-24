788ffd978c703b081c369213ea4e3aef0f313f95
jahan <jwalsh370@gmail.com>
2017-03-24 08:47:06 -0700

    add attribute for language and skill level. create getters and test both getters work correctly.

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,14 +1,26 @@
 public class Member {
 
   private String mName;
+  private String mLanguage;
+  private String mSkill;
 
-  public Member(String name) {
+  public Member(String name, String language, String skill) {
 
     mName = name;
+    mLanguage = language;
+    mSkill = skill;
   }
 
   public String getName() {
     return mName;
+  }
+
+  public String getLanguage() {
+    return mLanguage;
+  }
+
+  public String getSkill() {
+    return mSkill;
   }
 
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -7,14 +7,18 @@ public class MemberTest {
 
   @Test
   public void MemberClass_Instantiates_True() {
-    Member myMember = new Member("True That");
+    Member myMember = new Member("True That", "Java", "beginner");
     assertEquals(true, myMember instanceof Member);
   }
 
   @Test
   public void MemberClass_Test_Getters() {
-    Member myMember = new Member("Saul");
+    Member myMember = new Member("Saul", "Java", "beginner");
     assertEquals("Saul", myMember.getName());
+    assertEquals("Java", myMember.getLanguage());
+    assertEquals("beginner", myMember.getSkill());
   }
+
+
 
 }
